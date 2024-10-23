ac82ad34e6b1a2ae60bf28761b555ebd270056ed
Somondas <somondas067@gmail.com>
2024-10-23 14:24:41 +0530

    Navbar, Hero Section done

--- src/App.vue
+++ src/App.vue
@@ -1,1 +1,8 @@
-<script></script>
+<script setup>
+import Navbar from "@/components/Navbar.vue";
+import Hero from "@/components/Hero.vue";
+</script>
+<template>
+  <Navbar />
+  <Hero />
+</template>
